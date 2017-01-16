9023dd64e46bbb9945add5c1c198ea329e07e722
ytb33 <yannick.bostic@gmail.com>
2017-01-16 11:53:58 +0100

    Add files via upload

---
+++ navScroll/README.md
@@ -0,0 +1,101 @@
+# NavScroll
+
+NavScroll is a simple jQuery plugin for animated scrolling to a section of your page. It also features mobile dropdown support so if your navigation transforms into a dropdown on a certain breakpoint, navScroll hides the dropdown after a click.
+
+## Demo
+
+There's a demo included with this package. It's an example of navigation I use a lot, so feel free to use it yourself! <br>
+You can also checkout the demo here: [jhammann.github.io/jquery-navScroll/demo/](http://jhammann.github.io/jquery-navScroll/demo/)
+
+## Setup
+
+Include a recent version of jQuery and the navScroll plugin inside your page.
+
+```html
+<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="jquery.navScroll.js"></script>
+<script>
+  // To attach navScroll to an element without mobile dropdown support
+  $('.nav').navScroll();
+
+  // Or you can enable mobile dropdown support and give it a custom breakpoint (defaults to 1024)
+  $('.nav').navScroll({
+    mobileDropdown: true,
+    mobileBreakpoint: 768
+  });
+</script>
+```
+
+#### Install using bower
+**Optionally**, you can install jquery-navScroll with bower if you prefer:
+
+```shell
+bower install jquery-nav-scroll
+```
+
+## Options
+
+| Name | Description | Type | Default |
+|------|-------------|------|---------|
+| `scrollTime` | The time it takes to scroll to the element in milliseconds (set this to 0 so it obviously won't show an animation). | Integer | 500 |
+| `navItemClassName` | The class of the items which invokes the scroll animation. All anchor tags inside the element are clickable when the value is empty. | String | '' |
+| `navHeight` | Set the height of the navigation (to use as offset). 'auto' let's the plugin determine the height automatically, a number determines the height in px. | Integer or String | 'auto' |
+| `mobileDropdown` | If your navigation hides and is used as a dropdown on small screens setting this to true hides the dropdown after a click. | Boolean | false |
+| `mobileDropdownClassName` | Additionaly you can insert the mobile nav's classname here, when left empty the plugin searches for a `<ul>` in the same parent element. | String | '' |
+| `mobileBreakpoint` | The 'mobile' breakpoint (like the max-width of a media query). It's recommended that you check this if you use mobile dropdown support. | Integer | 1024 |
+| `scrollSpy` | Set to true if you want to enable the scrollspy, it adds an active class to the nav items when you scroll past their sections. | Boolean | false |
+| `activeParent` | Set to true if you want the parent of the anchor to have an active class instead of the anchor itself (only if ScrollSpy is enabled). | Boolean | false |
+| `activeClassName` | Set the name of the active class when using ScrollSpy. | String | 'active' |
+
+#### Specific scrollTime
+
+The scrollTime option is for all `<a>` tags inside the element on which navScroll is called. If you want different scrollTimes for different anchors you have to include the data-attribute `data-scrolltime`. The value should be a number. The 'Section 3' URL in the [demo](http://jhammann.github.io/jquery-navScroll/demo/) is an example of this.
+
+## Callbacks
+
+| Name | Description | Type |
+|------|-------------|------|
+| `onScrollStart` | Callback function, will be executed when the scrolling animation starts. | Function |
+| `onScrollEnd` | Callback function, will be executed when the scrolling animations ends. | Function |
+
+```js
+$('.nav').navScroll({
+  onScrollStart: function() {
+    // Execute code when the scrolling starts.
+  },
+  onScrollEnd: function() {
+    // Execute code when the scrolling ends.
+  }
+});
+```
+
+## Changelog
+
+#### Version 1.4.0
+* Added the `onScrollStart` and `onScrollEnd` callback functions.
+
+#### Version 1.3.1
+
+* Added the `activeClassName` option. This makes it possible to change the class name the plugin gives to an active item when using ScrollSpy.
+
+#### Version 1.3.0
+
+* Added the `activeParent` option. When set to true it gives the parent of the anchor an active class when using ScrollSpy instead of the anchor itself.
+* [Bugfix] Navigations with ScrollSpy enabled also support non-anchor URL's.
+
+#### Version 1.2.1
+
+* [Bugfix] ScrollSpy also works in IE. Added support for an older doctype.
+* Changed some styling for the demo.
+
+#### Version 1.2.0
+
+* Added the option to enable scrollSpy which gives nav items an *active* class when you scroll past their sections.
+
+#### Version 1.1.1
+
+* [Bugfix] Clicking on `<a>` tags without a leading `#` in the href attribute now navigates you to the actual path and won't return an error.
+
+#### Version 1.1.0
+
+* Added support for the scrollTime data attribute.
---
+++ navScroll/jquery.navScroll.js
@@ -0,0 +1,155 @@
+/*!
+ * NavScroll.js
+ * Version: 1.4.0
+ * Author: Jeroen Hammann
+ *
+ * Copyright (c) 2014 Jeroen Hammann
+ * Released under the MIT license
+*/
+
+;(function ($, window, document, undefined) {
+  'use strict';
+
+  var pluginName = 'navScroll',
+      defaults = {
+        // The time it takes to scroll to the element (set this to 0 so it obviously won't show an animation).
+        scrollTime: 500,
+        // The class of the items which invokes the scroll animation. All anchor tags inside the element are clickable when the value is empty.
+        navItemClassName: '',
+        // Set the height of the navigation (to use as offset). 'auto' let's the plugin determine the height automatically, a number determines the height in px.
+        navHeight: 'auto',
+        // If your navigation hides and is used as a dropdown on small screens setting this to true hides the dropdown after a click.
+        mobileDropdown: false,
+        // Additionaly you can insert the mobile nav's classname here, when left empty the plugin searches for a <ul> in the same parent element.
+        mobileDropdownClassName: '',
+        // The window width, which functions as a breakpoint between desktop and mobile.
+        mobileBreakpoint: 1024,
+        // Set to 'true' if you want to enable scrollspy.
+        scrollSpy: false,
+        // Set to true if you want the parent of the anchor to have an active class instead of the anchor itself (only if ScrollSpy is enabled).
+        activeParent: false,
+        // Set the name of the active class. When left blank the class name will be 'active'.
+        activeClassName: 'active'
+      };
+
+  function NavScroll(element, options) {
+    this.element = element;
+    this.options = $.extend({
+      onScrollStart: function() {},
+      onScrollEnd: function() {}
+    }, defaults, options);
+
+    this._defaults = defaults;
+    this._name = pluginName;
+
+    this.init();
+  }
+
+  NavScroll.prototype = {
+    init: function() {
+      var self, options, element, navItem, navOffset, scrollTime;
+      self = this;
+      options = self.options;
+      element = self.element;
+
+      if (options.navItemClassName === '') {
+        navItem = $(element).find('a');
+      } else {
+        navItem = $(element).find('.' + options.navItemClassName);
+      }
+
+      if (options.navHeight === 'auto') {
+        navOffset = $(element).height();
+      } else if (isNaN(options.navHeight)) {
+        throw new Error ('\'navHeight\' only accepts \'auto\' or a number as value.');
+      } else {
+        navOffset = options.navHeight;
+      }
+
+      navItem.on('click', function(e){
+        var url, parts, target, targetOffset, targetTop;
+
+        url = this.href;
+        parts = url.split('#');
+        target = parts[1];
+
+        if (target !== undefined) {
+          e.preventDefault();
+          targetOffset = $('#' + target).offset();
+          targetTop = targetOffset.top;
+        }
+
+        if ($(this).data('scrolltime') !== undefined) {
+          scrollTime = $(this).data('scrolltime');
+        } else {
+          scrollTime = options.scrollTime;
+        }
+
+        if (options.mobileDropdown && $(window).width() >= 0 && $(window).width() <= options.mobileBreakpoint) {
+          if (options.mobileDropdownClassName === '') {
+            $(element).find('ul').slideUp('fast');
+          } else {
+            $('.' + options.mobileDropdownClassName).slideUp('fast');
+          }
+        }
+
+        $('html, body').stop().animate({
+          scrollTop: targetTop - navOffset
+        }, scrollTime).promise().done(function() {
+          options.onScrollEnd.call(this);
+        });
+
+        options.onScrollStart.call(this);
+      });
+
+      if (options.scrollSpy) {
+        var scrollItems;
+        scrollItems = [];
+
+        navItem.each(function() {
+          var scrollItemId = $(this).attr('href');
+          if (scrollItemId.charAt(0) === '#') {
+            scrollItems.push($(scrollItemId));
+          }
+        });
+
+        $(window).on('scroll', function () {
+          self.scrollspy(options, navItem, scrollItems);
+        });
+        self.scrollspy(options, navItem, scrollItems);
+      }
+    },
+
+    scrollspy: function(options, navItem, scrollItems) {
+      var scrollPos, changeBounds, i, l;
+      scrollPos = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+      changeBounds = window.innerHeight / 2 || document.documentElement.clientHeight / 2;
+      l = navItem.length;
+
+      for (i = 0; l > i; i++) {
+        var item = scrollItems[i];
+        if (item !== undefined) {
+          if (scrollPos > (item.offset().top - changeBounds)) {
+            if (options.activeParent) {
+              navItem.parent().removeClass(options.activeClassName);
+              $(navItem[i]).parent().addClass(options.activeClassName);
+            } else {
+              navItem.removeClass(options.activeClassName);
+              $(navItem[i]).addClass(options.activeClassName);
+            }
+          }
+        }
+      }
+    }
+  };
+
+  $.fn[pluginName] = function (options) {
+    return this.each(function () {
+      if (!$.data(this, 'plugin_' + pluginName)) {
+        $.data(this, 'plugin_' + pluginName,
+        new NavScroll(this, options));
+      }
+    });
+  };
+
+})(jQuery, window, document);
